e8c3cd0dd92a04aa51a648dd717839eb0706d075
evrasouza@gmail.com <evrasouza@gmail.com>
2023-12-07 01:08:57 +0000

    stk-cli: create app initial commit.

---
+++ src/main/java/br/com/zup/notrevestudio/demo/DemoironApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.notrevestudio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoironApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoironApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/notrevestudio/demo/DemoironApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.notrevestudio.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoironApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
